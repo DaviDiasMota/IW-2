492b2ed30f78fe772ddb3dc052a225c0be8ff777
DaviDiasMota <ddmota2009@gmail.com>
2026-02-13 10:49:07 -0300

    Add files via upload

---
+++ exercicios_01/exercicio_003.js
@@ -0,0 +1,8 @@
+/* crie um programa que imprima a soma de todos os números de 1 á 15  usando while*/
+i = 1
+contador = 2
+while(contador != 16){
+    i += contador
+    contador ++
+    console.log(i)
+}
---
+++ exercicios_01/exercicio_004.js
@@ -0,0 +1,18 @@
+/* Desenvolva um programa que receba dois números, calcule a soma deles. Os resultados deveram ser multiplicados por 10. Ao final mostrar todas as etapas realizadas. Realizar estas etapas em sequência de 3 repetições, usando a estrutura for*/
+
+const prompt = require('prompt-sync')()
+let valor1 = prompt("Digite aqui o primeiro valor:  ")
+valor1 = parseFloat(valor1)
+let valor2 = prompt("Digite aqui o segundo valor:  ")
+valor2 = parseFloat(valor2)
+let soma = valor1 + valor2
+let mult = soma*10
+
+for (j = 0; j != 3; j++){
+    console.log(` O resultado da soma é ${soma}`)
+    console.log("------------------------")
+    console.log(`O resultado da sua soma por 10 é ${mult}`)
+    console.log("-------------------------------")
+}
+
+
